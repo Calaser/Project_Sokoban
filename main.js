8de50dd83888599e723bb21cfb8b78eb89d2fb9e
Calasa <s33003030@gmail.com>
2023-08-10 08:03:44 +0800

    Finish Basic Gameplay
    1.Fix the method that current map variable copy from map index.
    2.Add stage initial spawn spot.
    3.Add box and destination node and complete their interaction property.
    4.Now Store 3 real Sokoban Stage.
    5.Rebuild screen display:
    5-1.Use Taiwanese as stage structure element
    5-2.Screen is now fixed instead of moving with player.

--- main.js
+++ main.js
@@ -14,48 +14,94 @@ document.querySelector('#app').innerHTML = `
 </div>
 `;
 
-var mapIndex = 0;
-var locationX = 1;
-var locationY = 1;
-var map = [
+let mapIndex = 0;
+let locationX;
+let locationY;
+let checkIfFinish = false;
+const map = [
   [
-    [1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,1],
-    [1,0,2,0,0,0,1],
-    [1,0,0,0,4,0,1],
-    [1,0,3,0,0,0,1],
-    [1,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1]
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 1, 1],
+    [1, 3, 9, 2, 0, 0, 1, 1],
+    [1, 1, 1, 0, 2, 3, 1, 1],
+    [1, 3, 1, 1, 2, 0, 1, 1],
+    [1, 0, 1, 0, 3, 0, 1, 1],
+    [1, 2, 0, 4, 2, 2, 3, 1],
+    [1, 0, 0, 0, 3, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1]
   ],
   [
-    [1,1,1,1,1,1,1,1,1,1],
-    [1,0,2,0,0,0,4,0,0,1],
-    [1,0,2,0,3,0,4,0,0,1],
-    [1,0,2,0,3,0,0,0,0,1],
-    [1,0,2,0,3,0,4,0,0,1],
-    [1,0,0,0,3,0,4,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 2, 0, 0, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 1],
+    [1, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 9, 1, 1, 1, 1, 0, 0, 3, 3, 1],
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  ],
+  [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 3, 3, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 3, 3, 0, 0, 1, 0, 2, 0, 0, 2, 0, 0, 1],
+    [1, 3, 3, 0, 0, 1, 2, 1, 1, 1, 1, 0, 0, 1],
+    [1, 3, 3, 0, 0, 0, 0, 9, 0, 1, 1, 0, 0, 1],
+    [1, 3, 3, 0, 0, 1, 0, 1, 0, 0, 2, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 0, 2, 0, 1],
+    [1, 1, 1, 0, 2, 0, 0, 2, 0, 2, 0, 2, 0, 1],
+    [1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   ]
 ];
-var currentMap = map[mapIndex];
 
+let currentMap = JSON.parse(JSON.stringify(map[mapIndex]));
+
+for (var i = 0; i < currentMap.length; i++) {
+  for (var j = 0; j < currentMap[i].length; j++) {
+    if (currentMap[i][j] === 9) {
+      locationX = j;
+      locationY = i;
+      currentMap[i][j] = 0;
+    }
+  }
+  checkIfFinish = false;
+}
 
 document.querySelector('#decrementBtn').addEventListener("click", () => {
-  if(map[mapIndex-1]){
-    currentMap = map[--mapIndex];
-    locationX = 1;
-    locationY = 1;
-    document.querySelector('#stageCounter').innerHTML = `Stage ${mapIndex+1}`;
+  if (map[mapIndex - 1]) {
+    currentMap = JSON.parse(JSON.stringify(map[--mapIndex]));
+    for (var i = 0; i < currentMap.length; i++) {
+      for (var j = 0; j < currentMap[i].length; j++) {
+        if (currentMap[i][j] === 9) {
+          locationX = j;
+          locationY = i;
+          currentMap[i][j] = 0;
+        }
+      }
+    }
+    checkIfFinish = false;
+    document.querySelector('#stageCounter').innerHTML = `Stage ${mapIndex + 1}`;
     document.querySelector('#screen').innerHTML = printScreen(currentMap, locationX, locationY);
   }
 })
 
 document.querySelector('#incrementBtn').addEventListener("click", () => {
-  if(map[mapIndex+1]){
-    currentMap = map[++mapIndex];
-    locationX = 1;
-    locationY = 1;
-    document.querySelector('#stageCounter').innerHTML = `Stage ${mapIndex+1}`;
+  if (map[mapIndex + 1]) {
+    currentMap = JSON.parse(JSON.stringify(map[++mapIndex]));
+    for (var i = 0; i < currentMap.length; i++) {
+      for (var j = 0; j < currentMap[i].length; j++) {
+        if (currentMap[i][j] === 9) {
+          locationX = j;
+          locationY = i;
+          currentMap[i][j] = 0;
+        }
+      }
+    }
+    checkIfFinish = false;
+    document.querySelector('#stageCounter').innerHTML = `Stage ${mapIndex + 1}`;
     document.querySelector('#screen').innerHTML = printScreen(currentMap, locationX, locationY);
   }
 })
@@ -64,54 +110,138 @@ document.querySelector('#incrementBtn').addEventListener("click", () => {
 document.querySelector('#desc').innerHTML = `當前位置為(${locationX},${locationY})`;
 document.querySelector('#screen').innerHTML = printScreen(currentMap, locationX, locationY);
 window.addEventListener("keydown", (e) => {
-  switch(e.code){
+  switch (e.code) {
     case "KeyW":
     case "ArrowUp":
     case "Numpad8":
-      if(currentMap[locationY-1][locationX] === 0)
-        locationY--;
+      if (checkIfFinish) break;
+      move(0, -1);
       break;
     case "KeyS":
     case "ArrowDown":
     case "Numpad2":
-      if(currentMap[locationY+1][locationX] === 0)
-        locationY++;
+      if (checkIfFinish) break;
+      move(0, 1);
       break;
     case "KeyA":
     case "ArrowLeft":
     case "Numpad4":
-      if(currentMap[locationY][locationX-1] === 0)
-        locationX--;
+      if (checkIfFinish) break;
+      move(-1, 0);
       break;
     case "KeyD":
     case "ArrowRight":
     case "Numpad6":
-      if(currentMap[locationY][locationX+1] === 0)
-        locationX++;
+      if (checkIfFinish) break;
+      move(1, 0);
       break;
   }
   document.querySelector('#desc').innerHTML = `當前位置為(${locationX},${locationY})`;
   document.querySelector('#screen').innerHTML = printScreen(currentMap, locationX, locationY);
+  checkIfFinish = true;
+  for (var i = 0; i < currentMap.length; i++) {
+    for (var j = 0; j < currentMap[i].length; j++) {
+      if (currentMap[i][j] === 3) {
+        checkIfFinish = false;
+      }
+    }
+  }
 })
+
+function move(x, y) {
+  if (currentMap[locationY + y][locationX + x] === 0 || currentMap[locationY + y][locationX + x] === 3) {
+    if (x){
+      locationX+= x;
+      locationY+= y;
+    }
+    if (y){
+      locationX+= x;
+      locationY+= y;
+    }
+  }
+  else if (currentMap[locationY + y][locationX + x] === 2) {
+    if (currentMap[locationY + 2 * y][locationX + 2 * x] === 0) {
+      currentMap[locationY + y][locationX + x] = 0;
+      currentMap[locationY + 2 * y][locationX + 2 * x] = 2;
+        locationX+= x;
+        locationY+= y;
+    }
+    else if (currentMap[locationY + 2 * y][locationX + 2 * x] === 3) {
+      currentMap[locationY + y][locationX + x] = 0;
+      currentMap[locationY + 2 * y][locationX + 2 * x] = 4;
+      locationX+= x;
+      locationY+= y;
+    }
+  }
+  else if (currentMap[locationY + y][locationX + x] === 4) {
+    if (currentMap[locationY + 2 * y][locationX + 2 * x] === 0) {
+      currentMap[locationY + y][locationX + x] = 3;
+      currentMap[locationY + 2 * y][locationX + 2 * x] = 2;
+      locationX+= x;
+      locationY+= y;
+    }
+    else if (currentMap[locationY + 2 * y][locationX + 2 * x] === 3) {
+      currentMap[locationY + y][locationX + x] = 3;
+      currentMap[locationY + 2 * y][locationX + 2 * x] = 4;
+      locationX+= x;
+      locationY+= y;
+    }
+  }
+}
 
 function printScreen(map, locationX, locationY) {
   var screen = "";
-  var screenSize = 3;
-  for(let i = -screenSize; i <= screenSize; i++){
-    for(let j = -screenSize; j <= screenSize; j++){
-      if(locationY+i >= 0 && locationX+j >= 0 && map[locationY+i] !== undefined && map[locationY+i][locationX+j] !== undefined)
-        if(i === 0 && j === 0)
-          screen += "X";
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      if (i >= 0 && j >= 0 && map[i] !== undefined && map[i][j] !== undefined)
+        if (i === locationY && j === locationX)
+          screen += "我";
+        else if (map[i][j] === 0)
+          screen += "&emsp;";
+        else if (map[i][j] === 1)
+          screen += "牆";
+        else if (map[i][j] === 2)
+          screen += "口";
+        else if (map[i][j] === 3)
+          screen += "。";
+        else if (map[i][j] === 4)
+          screen += "回";
         else
-          screen += map[locationY+i][locationX+j];
+          screen += map[i][j];
       else
-        screen += "&nbsp;";
+        screen += "&emsp;";
 
-      if(j != screenSize)
-        screen += "&nbsp;&nbsp;&nbsp";
+      if (j != map[i].length - 1)
+        screen += "&ensp;";
       else
         screen += "<br>";
     }
   }
+
+  //  center character with partially map print style, use this in rpg or something like that
+  // 
+  // var screen = "";
+  // var screenSize = 4;
+  // 
+  // for(let i = -screenSize; i <= screenSize; i++){
+  //   for(let j = -screenSize; j <= screenSize; j++){
+  //     if(locationY+i >= 0 && locationX+j >= 0 && map[locationY+i] !== undefined && map[locationY+i][locationX+j] !== undefined)
+  //       if(i === 0 && j === 0)
+  //         screen += "。";
+  //       else if(map[locationY+i][locationX+j]===1)
+  //         screen += "牆";
+  //       else if(map[locationY+i][locationX+j]===0)
+  //       screen += "&emsp;";
+  //       else
+  //         screen += map[locationY+i][locationX+j];
+  //     else
+  //       screen += "&emsp;";
+
+  //     if(j != screenSize)
+  //       screen += "&emsp;";
+  //     else
+  //       screen += "<br>";
+  //   }
+  // }
   return screen;
 }
